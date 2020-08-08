b2651b169f987d1d7459cb62b902c9727c255358
Amon <i.amon@outlook.com>
2020-08-08 18:28:06 +0200

    If condition in runscript based on hardware

--- run.sh
+++ run.sh
@@ -2,13 +2,19 @@
 OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
-cd output
-mod_dir="wm8960_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
-for each in $mod_dir; do
-	echo Loading module from "$each"
-	#insmod "$each/snd-soc-wm8960-soundcard.ko"
-	insmod "$each/snd-soc-wm8960.ko"
-done
+is_1a=$(i2cdetect -y  1 0x1a 0x1a | egrep "(1a|UU)" | awk '{print $2}')
+
+if [ "x${is_1a}" != "x" ] ; then
+	echo "WM8960 soundcard found, loading kernel module"
+	cd output
+	mod_dir="wm8960_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+	for each in $mod_dir; do
+		echo Loading module from "$each"
+		#insmod "$each/snd-soc-wm8960-soundcard.ko"
+		insmod "$each/snd-soc-wm8960.ko"
+	done
+	alsactl restore
+fi
 
 while true; do
 	sleep 60
