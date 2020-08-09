3cb28690aab511580c42d549fe8d5ff72e356c3d
Amon <i.amon@outlook.com>
2020-08-09 16:35:57 +0200

    Do not insert module second time

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ is_1a=$(i2cdetect -y  1 0x1a 0x1a | egrep "(1a|UU)" | awk '{print $2}')
 if [ "x${is_1a}" != "x" ] ; then
 	echo "WM8960 soundcard found, loading kernel module"
 	cd output
-	mod_dir="wm8960_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+	mod_dir="wm8960_${BALENA_DEVICE_TYPE}_${OS_VERSION}"
 	for each in $mod_dir; do
 		echo Loading module from "$each"
 		#insmod "$each/snd-soc-wm8960-soundcard.ko"
